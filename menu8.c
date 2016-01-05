5ce0a1e6a58d2ea5d04e6dc2d7c8100c0ac2bb12
Dennis Binder <dennis.binder@gmx.de>
2016-01-06 00:46:05 +0100

    updated

---
+++ menu8.c
@@ -0,0 +1,35 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dispsw.h>
+
+#include "pangea_clock.h"
+
+//******************************************************************************
+// Menu8: Brigitte Computer
+//******************************************************************************
+
+
+void funcMenu8(UINT8 u8MenuNumber, UINT8 u8MenuValue, UINT8 u8Cmd)
+{
+	char txt[500]={0};
+	int pid;
+	switch(u8Cmd)
+	{
+		case DISP:
+		case FLASH:
+			dispsw_Set(u8MenuNumber/10, u8MenuNumber%10, u8MenuValue/10, u8MenuValue%10);
+			break;
+		case SET:
+			pid = fork();
+			if (pid == 0)
+			{
+				sprintf(txt, "/home/pi/musik/playmusic8 %d", u8MenuValue);
+				system(txt);
+				exit (1);
+			}
+			break;
+	}
+}
